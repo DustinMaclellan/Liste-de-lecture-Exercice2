6da9cf979a7d31f2c3400a9a03001a07fb6e5b04
Keven <keven.br2000@gmail.com>
2021-01-15 13:23:22 -0500

    Update FormulaireModifierPiece.js
    Modification du composant FormulaireModifierPiece afin de supporter plusieurs catégories dans une pièce.

--- liste-repertoire-frontend/src/composants/FormulaireModifierPiece.js
+++ liste-repertoire-frontend/src/composants/FormulaireModifierPiece.js
@@ -1,75 +1,113 @@
-import {
-    React,
-    useState,
-    useEffect
-} from 'react';
-
-import { Form } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import { Redirect } from 'react-router-dom';
+import { React, useState, useEffect } from "react";
+import { Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { Redirect } from "react-router-dom";
 
 function FormulaireModifierPiece({ id }) {
-    const [titre, setTitre] = useState('');
-    const [artiste, setArtiste] = useState('');
-    const [categorie, setCategorie] = useState('');
-    const [rediriger, setRediriger] = useState(false);
+  const [titre, setTitre] = useState("");
+  const [artiste, setArtiste] = useState("");
+  const [categories, setCategories] = useState([""]);
+  const [rediriger, setRediriger] = useState(false);
 
-    useEffect(() => {
-        const chercherDonnees = async () => {
-            const resultat = await fetch(`/api/pieces/${id}`);
-            const body = await resultat.json().catch((error) => {console.log(error)});
-            setTitre(body.titre);
-            setArtiste(body.artiste);
-            setCategorie(body.categorie);
-        };
-        chercherDonnees();
-    }, [id]);
-
-    const envoyerFormulaire = async () => {
-        await fetch(`/api/pieces/modifier/${id}`, {
-            method: 'post',
-            body: JSON.stringify({ titre, artiste, categorie }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        setRediriger(true);
+  useEffect(() => {
+    const chercherDonnees = async () => {
+      const resultat = await fetch(`/api/pieces/${id}`);
+      const body = await resultat.json().catch((error) => {
+        console.log(error);
+      });
+      setTitre(body.titre);
+      setArtiste(body.artiste);
+      setCategories(body.categories);
     };
+    chercherDonnees();
+  }, [id]);
 
-    function AfficherRedirection() {
-        if (rediriger === true) {
-            return <Redirect to="/admin" />
-        }
+  const envoyerFormulaire = async () => {
+    await fetch(`/api/pieces/modifier/${id}`, {
+      method: "post",
+      body: JSON.stringify({ titre, artiste, categories }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setRediriger(true);
+  };
+
+  function AfficherRedirection() {
+    if (rediriger === true) {
+      return <Redirect to="/admin" />;
     }
-    
-    return (
+  }
+
+  const handleClickRemove = (index) => {
+    const list = [...categories];
+    list.splice(index, 1);
+    setCategories(list);
+  };
+
+  const handleClickAdd = () => {
+    setCategories([...categories, ""]);
+  };
+
+  const handleInputChange = (e, index) => {
+    const valeur = e.target.value;
+    const list = [...categories];
+    list[index] = valeur;
+    setCategories(list);
+  };
+
+  return (
     <>
-        {AfficherRedirection()}
-        <Form className="mb-1">
-            <Form.Group>
-                <Form.Label>Titre</Form.Label>
-                <Form.Control type="text" value={titre} 
-                    onChange={(event) => setTitre(event.target.value)} />
-            </Form.Group>
+      {AfficherRedirection()}
+      <Form className="mb-1">
+        <Form.Group>
+          <Form.Label>Titre</Form.Label>
+          <Form.Control
+            type="text"
+            value={titre}
+            onChange={(event) => setTitre(event.target.value)}
+          />
+        </Form.Group>
 
-            <Form.Group>
-                <Form.Label>Artiste / Groupe</Form.Label>
-                <Form.Control type="text" value={artiste} 
-                    onChange={(event) => setArtiste(event.target.value)} />
-            </Form.Group>
+        <Form.Group>
+          <Form.Label>Artiste / Groupe</Form.Label>
+          <Form.Control
+            type="text"
+            value={artiste}
+            onChange={(event) => setArtiste(event.target.value)}
+          />
+        </Form.Group>
 
+        <Form.Label>Catégories</Form.Label>
+        {categories.map((valeur, index) => {
+          return (
             <Form.Group>
-                <Form.Label>Catégorie</Form.Label>
-                <Form.Control type="text" value={categorie} 
-                    onChange={(event) => setCategorie(event.target.value)} />
-            </Form.Group>
+              <Form.Control
+                type="text"
+                value={valeur}
+                onChange={(event) => handleInputChange(event, index)}
+              />
 
-            <Button variant="success" onClick={envoyerFormulaire} >
-                Modifier
-            </Button>
-        </Form>
+              {categories.length != 1 && (
+                <Button
+                  variant="danger"
+                  onClick={() => handleClickRemove(index)}
+                >
+                  Supprimer
+                </Button>
+              )}
+            </Form.Group>
+          );
+        })}
+        <Button onClick={handleClickAdd}>Ajouter Une Catégorie</Button>
+        <br />
+        <br />
+        <Button variant="success" onClick={envoyerFormulaire}>
+          Modifier
+        </Button>
+      </Form>
     </>
-    );
+  );
 }
 
 export default FormulaireModifierPiece;
